4adf1302debe8cb341b83cd7d931910283070704
Miku Kawata <kawata_miku@lightcafe.co.jp>
2024-03-04 11:58:54 +0900

    feat: LPを作成

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,21 +5,29 @@ import styles from './style.module.css';
 const Header: FC = () => {
   return (
     <nav className={styles.nav}>
-      <Link href='/janken' className={styles.link}>
-        じゃんけん
-      </Link>
-      <Link href='#' className={styles.link}>
-        ゲーム
-      </Link>
-      <Link href='#' className={styles.link}>
-        ゲーム
-      </Link>
-      <Link href='#' className={styles.link}>
-        ゲーム
-      </Link>
-      <Link href='#' className={styles.link}>
-        ゲーム
-      </Link>
+      <div className={styles.home}>
+        <Link href='/' className={styles.link}>
+          Game app
+        </Link>
+      </div>
+
+      <div className={styles.links}>
+        <Link href='/janken' className={styles.link}>
+          じゃんけん
+        </Link>
+        <Link href='#' className={styles.link}>
+          作成中
+        </Link>
+        <Link href='#' className={styles.link}>
+          作成中
+        </Link>
+        <Link href='#' className={styles.link}>
+          作成中
+        </Link>
+        <Link href='#' className={styles.link}>
+          作成中
+        </Link>
+      </div>
     </nav>
   );
 };
