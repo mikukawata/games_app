a9798eaf15f83b7a0aefd66431c4ad9fb121e25c
Miku Kawata <kawata_miku@lightcafe.co.jp>
2024-03-04 13:46:07 +0900

    feat: スコアUP機能を実装

--- src/app/janken/page.tsx
+++ src/app/janken/page.tsx
@@ -1,15 +1,20 @@
 'use client';
 import React, { useState } from 'react';
 import styles from './page.module.css';
-import Score from '@/components/Janken/Score';
 import Result from '@/components/Janken/Result';
 import ChooseBtn from '@/components/Janken/ChooseBtn';
 import { NextPage } from 'next';
 import { SelectedChoise } from '@/components/Janken/SelectedChoise';
+import Score from '@/components/Janken/Score';
 
 export interface Choise {
   value: string;
   emoji: string;
+}
+
+export interface Scores {
+  yourScore: number;
+  comScore: number;
 }
 
 const Janken: NextPage = () => {
@@ -17,9 +22,10 @@ const Janken: NextPage = () => {
   const [comChoiseIdx, setComChoiseIdx] = useState<number | undefined>();
   const [result, setResult] = useState<number>(3);
   const [active, setActive] = useState<boolean>(false);
+  const [score, setScore] = useState<Scores>({ yourScore: 0, comScore: 0 });
 
   const resultMsg = ['あいこです。', '勝ちました 🎉', '負けました...', ''];
-  const choices = [
+  const choices: Choise[] = [
     { value: 'rock', emoji: '✊' },
     { value: 'scissors', emoji: '✌️' },
     { value: 'paper', emoji: '🖐️' },
@@ -38,14 +44,22 @@ const Janken: NextPage = () => {
     setActive(true);
     setTimeout(() => {
       setActive(false);
-    }, 1000);
+    }, 800);
+
+    setScore((prevScore) => {
+      if (calcResult !== 0) {
+        const player = calcResult === 1 ? 'yourScore' : 'comScore';
+        return { ...prevScore, [player]: prevScore[player] + 1 };
+      }
+      return prevScore;
+    });
   };
 
   return (
     <div className={styles.game}>
       <h1 className={styles.title}>じゃんけんゲーム</h1>
 
-      <Score />
+      <Score score={score} />
       <div className={styles.container}>
         <SelectedChoise user='あなた' choise={yourChoiseIdx ? choices[yourChoiseIdx].emoji : '✊'} active={active} />
         <SelectedChoise user='コンピュータ' choise={comChoiseIdx ? choices[comChoiseIdx].emoji : '✊'} active={active} />
